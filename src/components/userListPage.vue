82b5b2863219ba9b1d1815fb0b1eba4164798a30
BuildTools <unconfigured@null.spigotmc.org>
2022-09-08 06:34:53 +0900

    axios first

---
+++ src/components/userListPage.vue
@@ -0,0 +1,61 @@
+<template>
+    <div v-if="userList === null" class="loader">
+          <img width="70" src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/04de2e31234507.564a1d23645bf.gif" />
+          <h4>데이터를 불러오는 중입니다.</h4>
+      </div>
+  
+      <div v-else-if="userList.length < 1" class="loader">
+          <h4>
+              <img width="40" src="https://cdn-icons-png.flaticon.com/512/179/179386.png?w=360" />
+              유저가 발견되지 않았습니다.
+          </h4>
+      </div>
+  
+      <div v-else>
+          <ul>
+              <li v-for="(user,idx) in userList" :key="idx">
+                  <router-link :to="{ name: 'userProfile', params: { userId : idx } }"><li><h4>{{ user.name }}</h4></li></router-link>
+                  <p>({{ user.address.zipcode }}) {{ user.address.street }}</p>
+              </li>
+          </ul>
+      </div>
+      <button @click="fetchData()">새로고침</button>
+  </template>
+  
+  <script>
+  export default {
+      data() {
+          return {
+              userList : null
+          }
+      },
+      methods: {
+          fetchData: function() {
+              let self = this;
+              this.$axios.get('https://jsonplaceholder.typicode.com/users')
+              .then(function(response) {
+                  // response
+                  if(response.status !== 200){
+                      alert('통신에러!');
+                      self.userList = [];
+                  }else{
+                      self.userList = response.data;
+                  }
+              })
+          }
+      },
+      mounted() {
+          this.fetchData();
+      },
+  }
+  </script>
+  
+  <style>
+  .loader{
+      text-align:center;
+      padding:40px 0;
+      border:1px solid #eee;
+      border-radius:10px;
+      margin:40px;
+  }
+  </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,32 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory}from 'vue-router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import Axios from 'axios';
+import home from './components/homePage.vue';
+import userList from './components/userListPage.vue';
+import userProfile from './components/userProfilePage.vue';
+
+const routes = [
+    { name:'home', path: '/', component: home },
+    { name:'userList', path: '/user', component: userList },
+    { 
+        path: '/user', 
+        component: userProfile,
+        children: [
+            {name:'userProfile', path: ':userId', component: userProfile}
+        ]
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
+const app = createApp(App)
+app.use(router)
+
+app.mount('#app')
+
+app.config.globalProperties.$axios = Axios;
